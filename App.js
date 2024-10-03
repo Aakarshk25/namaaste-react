378da11f4d9a8f328f336b4943c06ea0133f8def
Aakarshk25 <aakarshkumar495@gmail.com>
2024-10-03 15:51:47 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+const heading = React.createElement("h1",{id:"heading"},"hello world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);//object is heading
+root.render(heading);
